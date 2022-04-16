fec21550c76bba77ca28c6b5ed0ed7eb2c57bebb
Edoki Chukwuyem <edokichuks@gmail.com>
2022-04-16 06:35:33 -0700

    5-more_numbers.c task

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,11 +11,10 @@ int i, j;
 
 for (i = 0; i < 10; i++)
 {
-for (j = 0; j < 15; j++)
+	for (j = 0; j < 15; j++)
 {
 if (j >= 10)
-_putchar(j / 10 + '0');		
-
+	_putchar(j / 10 + '0');
 _putchar(j % 10 + '0');
 }
 _putchar('\n');
