019b5ec8e66f2e225f2f2d713cd4e2b4c88780e0
Edoki Chukwuyem <edokichuks@gmail.com>
2022-04-16 06:45:04 -0700

    task7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,24 +6,40 @@
 */
 void print_diagonal(int n)
 {
-	if (n <= 0)
-	{
-		_putchar('\n');
-	} else
+	
+if (n <= 0)
+
+{
+
+	_putchar('\n');
+
+}
+else
+
+{
+	int i, j;
+	for (i = 0; i < n; i++)
+	
 	{
 		
-		int i, j;
-		
-		for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++)
+
 		{
-			for (j = 0; j < n; j++)
-			{
-				if (j == i)
-					_putchar('\\');
-				else if (j < i)
-					_putchar(' ');
-			}
-			_putchar('\n');
+			
+			if (j == i)
+				
+				_putchar('\\');
+			
+			else if (j < i)
+				
+				_putchar(' ');
+		
 		}
+		
+		_putchar('\n');
+	
 	}
+
+}
+
 }
