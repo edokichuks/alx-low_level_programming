cece53f4a5ea7b85dc61fb484f5728e9822cf67c
Edoki Chukwuyem <edokichuks@gmail.com>
2022-04-16 05:52:56 -0700

    5-more_numbers.c task

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* more_numbers - prints 10 times the numbers, from 0 to 14
+* more_numbers - prints 10 times the numbers, from 0 to 14 and okay
 * followed by a new line
 */
 
