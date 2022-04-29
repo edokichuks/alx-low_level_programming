1e97936844573ca1525285e32f6d08cc8039c536
Edoki Chukwuyem <edokichuks@gmail.com>
2022-04-28 21:38:49 -0700

    task7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -6,7 +6,7 @@
  * Return: recursion
  */
 
-int is_palindrome(char *s)
+int _strlen_recursion(char *s)
 {
 	if (!*s)
 	{
@@ -44,7 +44,7 @@ int p1(char *s, int l)
 
 int is_palindrome(char *s)
 {
-	int len = _strlen_recursion(s):
+	int len = _strlen_recursion(s);
 
 	return (p1(s, len - 1));
 }
