bd8535ef74edff83fa61aae178cd889ed57b2722
Edoki Chukwuyem <edokichuks@gmail.com>
2022-04-28 20:55:11 -0700

    task1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string
+ * Returnn: no return
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_put_rev_recuresion(s + 1);
+		_putchar(*s);
+	}
+}
