36a03eec1e68e24f5706b4ccb1efba3366636e7d
Edoki Chukwuyem <edokichuks@gmail.com>
2022-04-29 21:49:32 -0700

    task0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - files memory with a constant key
+ * @s: memory area
+ * @b: constant btye
+ * @n: bytes filled
+ * Return: the pointer to dest
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(s + i) = b;
+
+	return (s);
+}
