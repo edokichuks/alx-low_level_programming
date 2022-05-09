44de2bc27e25defe3d1fecefb20473ac297cd18f
Edoki Chukwuyem <edokichuks@gmail.com>
2022-05-09 12:12:13 -0700

    task0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdlib>
+#include "main.h"
+
+/**
+ * malloc_checked - allocated memory using malloc
+ * @b: size of memoory to be allocated
+ * Return: pointer to the allocated memory
+ * 98 if insufficent memory was available
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+
+	return (p);
+}
