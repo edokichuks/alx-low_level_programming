72181ee8d005d8a0b61b535830172d62f19659b9
Edoki Chukwuyem <edokichuks@gmail.com>
2022-05-11 21:50:54 -0700

    task update

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+
+/**
+ * main: prints the name of the file
+ *
+ * main - prints
+ * main: this tis the decription
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+this is tis the readme file
