8f21acc2dc1fc54d4b8e9b26065390d5ab21fc66
edokichuks <92238247+edokichuks@users.noreply.github.com>
2022-05-11 21:58:38 -0700

    bug fix
    update

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,12 @@
-#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * main: prints the name of the file
+ * main - prints the name of the file.
  *
- * Return: always 0
+ * Return: Always 0.
  */
-
 int main(void)
 {
-	printf("%s\n", _FILE_);
+	printf("%s\n", __FILE__);
 	return (0);
 }
