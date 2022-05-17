1f8f8b0f76f0705bcba18b68d71b684dd372ca63
edokichuks <92238247+edokichuks@users.noreply.github.com>
2022-05-17 05:30:21 +0100

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,26 +1,25 @@
 #ifndef _FUNCTION_VARIADIC_H
 #define _FUNCTION_VARIADIC_H
 
-int sum_them_all(const unsigned int n, ...);
-
-void print_numbers(const char *separator, const unsigned int n, ...);
-
-void print_strings(const char *separator, const unsigned int n, ...);
-
-void print_all(const char * const format, ...);
-
 #include <stdarg.h>
 
 /**
- * struct print - multiple choice print
- * @x: char Type of print
- * @T_func: funct
+ * struct printer - multiple choice print
+ * @symbol: A symbol representing a data type.
+ * @print: function pointer to a function that prints a data type corresding to symbol
  */
 
-typedef struct print 
+typedef struct printer 
 
 {
-	char *x;
-	void (*T_func)(va_list);
-}t_print
+	char *symbol;
+	void (*print)(va_list arg);
+	
+}printer_t;
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
 #endif
