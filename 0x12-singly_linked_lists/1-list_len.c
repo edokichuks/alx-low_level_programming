f350e16d60122724c9805945bc9e710ef7396305
Edoki Chukwuyem <edokichuks@gmail.com>
2022-05-24 20:48:37 -0700

    task1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the no of elements in a list
+ * @h: singly linked list
+ * Return: no of elements in the list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t nelem;
+
+	nelem = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		nelem++;
+	}
+	return (nelem);
+}
