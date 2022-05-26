bcaa710e2f50f40601a034a5f524ca057875c936
Edoki Chukwuyem <edokichuks@gmail.com>
2022-05-25 21:40:03 -0700

    task doone

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * bmain: - function executed before main
+ * _attribute - this is a void method
+ * Return: void
+ */
+
+void _attribute_(constructor) bmain()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(", \nI bore mye house upon my back!\n");
+}
